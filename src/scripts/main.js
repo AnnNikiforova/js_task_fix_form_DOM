001a2c163482c1c78101f8011a049d9f2afad1ee
Anna <annanicky90gmail.com>
2024-10-18 12:34:49 +0400

    fix auth form

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write code here
+document.querySelectorAll('form').forEach((form) => {
+  const inputs = form.querySelectorAll('input');
+
+  inputs.forEach((input) => {
+    const parentField = input.closest('.field');
+    const label = document.createElement('label');
+
+    label.classList.add('field-label');
+    label.setAttribute('for', input.id);
+
+    label.textContent =
+      input.name.charAt(0).toUpperCase() + input.name.slice(1);
+
+    input.placeholder = `Enter your ${label.textContent}`;
+
+    parentField.insertBefore(label, input);
+  });
+});
